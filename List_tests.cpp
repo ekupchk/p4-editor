10ea82714428828737e896eaeacb0fd4b52b9be9
Elijah Uribe <euribe@umich.edu>
2024-03-24 20:16:47 -0400

    finished up the first set of function test cases

--- List_tests.cpp
+++ List_tests.cpp
@@ -5,9 +5,70 @@ using namespace std;
 
 // Add your test cases here
 
-TEST(test_stub) {
-    // Add test code here
-    ASSERT_TRUE(true);
+TEST(testing_empty) {
+    List<int> emptyList;
+    ASSERT_TRUE(emptyList.empty());
 }
+
+TEST(testing_size) {
+    List<int> emptyList;
+    ASSERT_EQUAL(emptyList.size(),0);
+
+    emptyList.push_front(10);
+    ASSERT_EQUAL(emptyList.size(),1);
+}
+
+TEST(testing_front){
+    List<int> emptyList;
+    ASSERT_EQUAL(emptyList.front(),nullptr);
+
+    emptyList.push_front(10);
+    ASSERT_EQUAL(emptyList.front(),10);
+}
+
+TEST(testing_back){
+    List<int> emptyList;
+    ASSERT_EQUAL(emptyList.back(),nullptr);
+
+    emptyList.push_back(10);
+    ASSERT_EQUAL(emptyList.back(),10);
+}
+
+TEST(testing_pushfront){
+    List<int> emptyList;
+
+    emptyList.push_front(10);
+    ASSERT_EQUAL(emptyList.front(),10);
+
+    emptyList.push_front(20);
+    ASSERT_EQUAL(emptyList.front(),20);
+}
+
+TEST(testing_pushback){
+    List<int> emptyList;
+
+    emptyList.push_back(10);
+    ASSERT_EQUAL(emptyList.back(),10);
+
+    emptyList.push_back(20);
+    ASSERT_EQUAL(emptyList.back(),20);
+}
+
+TEST(testing_popfront){
+    List<int> emptyList;
+    emptyList.push_front(10);
+    emptyList.push_front(20);
+    emptyList.pop_front();
+    ASSERT_EQUAL(emptyList.front(),10);
+}
+
+TEST(testing_popback){
+    List<int> emptyList;
+    emptyList.push_front(10);
+    emptyList.push_front(20);
+    emptyList.pop_back();
+    ASSERT_EQUAL(emptyList.front(),20);
+}
+
 
 TEST_MAIN()
