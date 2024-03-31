7e2d312c8f5c61167cd603f45d642c0cf692ad89
ekupchk <ekupchik@umich.edu>
2024-03-31 15:22:35 -0400

    Update List_tests.cpp

--- List_tests.cpp
+++ List_tests.cpp
@@ -5,10 +5,59 @@ using namespace std;
 
 // Add your test cases here
 
-TEST(testing_empty) {
+//Empty execution
+TEST(testing_empty_default) {
+    //Checking consecuitve executions 
     List<int> emptyList;
     ASSERT_TRUE(emptyList.empty());
+    ASSERT_TRUE(emptyList.empty());
+    //std::cout << "Consecutive test of default empty passed" << std::endl;
+
+    List<int> emptyList2;
+    ASSERT_TRUE(emptyList2.empty());
+    for (int i = 0; i < 5; ++i) {
+        emptyList2.push_back(i);
+    }
+    ASSERT_FALSE(emptyList2.empty());
+    ASSERT_FALSE(emptyList2.empty());
+    //std::cout << "Test of default empty pre and post filled passed" << std::endl;
+
+    int j = 0;
+    for (List<int>::Iterator it = emptyList2.begin(); it != emptyList2.end(); ++it, ++j) {
+        //std::cout << "At item #" << j << ", get " << *it << std::endl;
+        ASSERT_EQUAL(*it, j);
+    }
+
+    //std::cout << "Test of prefilled list variable modifications passed" << std::endl;
 }
+
+//Copy constructor execution
+TEST(testing_empty_default) {
+    //Checking consecuitve executions 
+    List<int> base_maker;
+    List<int> emptyList = base_maker;
+    ASSERT_TRUE(emptyList.empty());
+    ASSERT_TRUE(emptyList.empty());
+    //std::cout << "Consecutive test of copy empty passed" << std::endl;
+
+    ASSERT_TRUE(base_maker.empty());
+    for (int i = 0; i < 5; ++i) {
+        base_maker.push_back(i);
+    }
+    List<int> emptyList2 = base_maker;
+    ASSERT_FALSE(emptyList2.empty());
+    ASSERT_FALSE(emptyList2.empty());
+    //std::cout << "Test of copy empty pre and post filled passed" << std::endl;
+
+    int j = 0;
+    for (List<int>::Iterator it = emptyList2.begin(); it != emptyList2.end(); ++it, ++j) {
+        //std::cout << "At item #" << j << ", get " << *it << std::endl;
+        ASSERT_EQUAL(*it, j);
+    }
+
+    //std::cout << "Test of prefilled list variable modifications passed" << std::endl;
+}
+
 
 TEST(testing_size) {
     List<int> emptyList;
